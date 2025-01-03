2fff515a8db76ec86460109c1de8c8dfa8585a39
grumbach <anselmega@gmail.com>
2025-01-03 22:39:49 +0900

    feat: remove feat flags from ci

--- autonomi/tests/fs.rs
+++ autonomi/tests/fs.rs
@@ -18,7 +18,7 @@ use tokio::time::sleep;
 use walkdir::WalkDir;
 
 // With a local evm network, and local network, run:
-// EVM_NETWORK=local cargo test --features="fs,local" --package autonomi --test file
+// EVM_NETWORK=local cargo test --features="local" --package autonomi --test fs
 #[tokio::test]
 async fn dir_upload_download() -> Result<()> {
     let _log_appender_guard =

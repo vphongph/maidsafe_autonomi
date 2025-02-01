77a71499c5a4f0cab6c8aeb28593fea89546bc81
Chris O'Neil <chriso83@protonmail.com>
2025-02-01 12:48:57 +0000

    chore(release): release candidate 2025.1.1.1
    ======================
      New Crate Versions
    ======================
    ant-bootstrap: 0.1.5-rc.1
    ant-build-info: 0.1.24-rc.1
    ant-cli: 0.3.7-rc.1
    ant-evm: 0.1.9-rc.1
    ant-logging: 0.2.46-rc.1
    ant-metrics: 0.1.25-rc.1
    ant-networking: 0.3.5-rc.1
    ant-node: 0.3.6-rc.1
    ant-node-manager: 0.11.8-rc.1
    ant-node-rpc-client: 0.6.42-rc.1
    ant-protocol: 0.3.4-rc.1
    ant-service-management: 0.4.8-rc.1
    ant-token-supplies: 0.1.63-rc.1
    autonomi: 0.3.6-rc.1
    evmlib: 0.1.9-rc.1
    evm-testnet: 0.1.9-rc.1
    nat-detection: 0.2.16-rc.1
    node-launchpad: 0.5.4-rc.1
    test-utils: 0.4.16-rc.1
    =======================
      New Binary Versions
    =======================
    ant: 0.3.7-rc.1
    antctl: 0.11.8-rc.1
    antctld: 0.11.8-rc.1
    antnode: 0.3.6-rc.1
    antnode_rpc_client: 0.6.42-rc.1
    nat-detection: 0.2.16-rc.1
    node-launchpad: 0.5.4-rc.1

--- ant-build-info/src/release_info.rs
+++ ant-build-info/src/release_info.rs
@@ -1,4 +1,4 @@
-pub const RELEASE_YEAR: &str = "2024";
-pub const RELEASE_MONTH: &str = "12";
+pub const RELEASE_YEAR: &str = "2025";
+pub const RELEASE_MONTH: &str = "1";
 pub const RELEASE_CYCLE: &str = "1";
-pub const RELEASE_CYCLE_COUNTER: &str = "11";
+pub const RELEASE_CYCLE_COUNTER: &str = "1";

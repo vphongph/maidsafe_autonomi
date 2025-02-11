f7570bde2816d2dc371caec744f01f73a54ecb66
Roland Sherwin <RolandSherwin@protonmail.com>
2025-02-11 17:21:58 +0530

    Revert "feat(network): enable dcutr"
    This reverts commit de50e52c45001805d3ea4d0cec517e34ff8d3580.

--- ant-networking/src/driver.rs
+++ ant-networking/src/driver.rs
@@ -154,7 +154,6 @@ pub(super) struct NodeBehaviour {
     pub(super) upnp: Toggle<libp2p::upnp::tokio::Behaviour>,
     pub(super) relay_client: libp2p::relay::client::Behaviour,
     pub(super) relay_server: libp2p::relay::Behaviour,
-    pub(super) dcutr: libp2p::dcutr::Behaviour,
     pub(super) kademlia: kad::Behaviour<UnifiedRecordStore>,
     pub(super) request_response: request_response::cbor::Behaviour<Request, Response>,
 }
@@ -546,7 +545,6 @@ impl NetworkBuilder {
             relay_server,
             upnp,
             request_response,
-            dcutr: libp2p::dcutr::Behaviour::new(peer_id),
             kademlia,
             identify,
         };

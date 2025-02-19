2c8454c7e3a09f059c77f39d6fa5b653603f575c
grumbach <anselmega@gmail.com>
2025-02-19 12:50:20 +0900

    feat: support direct download of files or datamaps in the CLI

--- ant-cli/src/actions/download.rs
+++ ant-cli/src/actions/download.rs
@@ -8,10 +8,13 @@
 
 use super::get_progress_bar;
 use autonomi::{
+    chunk::DataMapChunk,
     client::{
-        address::str_to_addr, files::archive_private::PrivateArchiveAccess,
-        files::archive_public::ArchiveAddr,
+        address::str_to_addr,
+        files::{archive_private::PrivateArchiveAccess, archive_public::ArchiveAddr},
+        GetError,
     },
+    data::DataAddr,
     Client,
 };
 use color_eyre::{
@@ -21,19 +24,25 @@ use color_eyre::{
 use std::path::PathBuf;
 
 pub async fn download(addr: &str, dest_path: &str, client: &Client) -> Result<()> {
-    let public_address = str_to_addr(addr).ok();
-    let private_address = crate::user_data::get_local_private_archive_access(addr)
-        .inspect_err(|e| error!("Failed to get private archive access: {e}"))
-        .ok();
+    let try_public_address = str_to_addr(addr).ok();
+    if let Some(public_address) = try_public_address {
+        return download_public(addr, public_address, dest_path, client).await;
+    }
 
-    match (public_address, private_address) {
-        (Some(public_address), _) => download_public(addr, public_address, dest_path, client).await,
-        (_, Some(private_address)) => download_private(addr, private_address, dest_path, client).await,
-        _ => Err(eyre!("Failed to parse data address {addr}"))
+    let try_private_address = crate::user_data::get_local_private_archive_access(addr).ok();
+    if let Some(private_address) = try_private_address {
+        return download_private(addr, private_address, dest_path, client).await;
+    }
+
+    let try_datamap = DataMapChunk::from_hex(addr).ok();
+    if let Some(datamap) = try_datamap {
+        return download_from_datamap(addr, datamap, dest_path, client).await;
+    }
+
+    Err(eyre!("Failed to parse data address {addr}"))
             .with_suggestion(|| "Public addresses look like this: 0037cfa13eae4393841cbc00c3a33cade0f98b8c1f20826e5c51f8269e7b09d7")
             .with_suggestion(|| "Private addresses look like this: 1358645341480028172")
-            .with_suggestion(|| "Try the `file list` command to get addresses you have access to"),
-    }
+            .with_suggestion(|| "Try the `file list` command to get addresses you have access to")
 }
 
 async fn download_private(
@@ -45,7 +54,7 @@ async fn download_private(
     let archive = client
         .archive_get(&private_address)
         .await
-        .wrap_err("Failed to fetch data from address")?;
+        .wrap_err("Failed to fetch Private Archive from address")?;
 
     let progress_bar = get_progress_bar(archive.iter().count() as u64)?;
     let mut all_errs = vec![];
@@ -88,10 +97,16 @@ async fn download_public(
     dest_path: &str,
     client: &Client,
 ) -> Result<()> {
-    let archive = client
-        .archive_get_public(&address)
-        .await
-        .wrap_err("Failed to fetch data from address")?;
+    let archive = match client.archive_get_public(&address).await {
+        Ok(archive) => archive,
+        Err(GetError::Deserialization(_)) => {
+            info!("Failed to deserialize Public Archive from address, trying to fetch data assuming it is a single file instead");
+            return download_public_single_file(addr, address, dest_path, client)
+                .await
+                .wrap_err("Failed to fetch public file from address");
+        }
+        Err(e) => return Err(e).wrap_err("Failed to fetch Public Archive from address")?,
+    };
 
     let progress_bar = get_progress_bar(archive.iter().count() as u64)?;
     let mut all_errs = vec![];
@@ -126,4 +141,52 @@ async fn download_public(
         error!("Errors while downloading data at {addr}: {all_errs:#?}");
         Err(eyre!("Errors while downloading data"))
     }
+}
+
+async fn download_public_single_file(
+    addr: &str,
+    address: DataAddr,
+    dest_path: &str,
+    client: &Client,
+) -> Result<()> {
+    let bytes = match client.data_get_public(&address).await {
+        Ok(bytes) => bytes,
+        Err(e) => {
+            let err = format!("Failed to fetch file at {addr:?}: {e}");
+            return Err(eyre!(err)).wrap_err("Failed to fetch file content from address");
+        }
+    };
+
+    let path = PathBuf::from(dest_path);
+    let here = PathBuf::from(".");
+    let parent = path.parent().unwrap_or_else(|| &here);
+    std::fs::create_dir_all(parent)?;
+    std::fs::write(path, bytes)?;
+    info!("Successfully downloaded file at: {addr}");
+    println!("Successfully downloaded file at: {addr}");
+    Ok(())
+}
+
+async fn download_from_datamap(
+    addr: &str,
+    datamap: DataMapChunk,
+    dest_path: &str,
+    client: &Client,
+) -> Result<()> {
+    let bytes = match client.data_get(&datamap).await {
+        Ok(bytes) => bytes,
+        Err(e) => {
+            let err = format!("Failed to fetch file {addr:?}: {e}");
+            return Err(eyre!(err)).wrap_err("Failed to fetch file content from address");
+        }
+    };
+
+    let path = PathBuf::from(dest_path);
+    let here = PathBuf::from(".");
+    let parent = path.parent().unwrap_or_else(|| &here);
+    std::fs::create_dir_all(parent)?;
+    std::fs::write(path, bytes)?;
+    info!("Successfully downloaded file from datamap at: {addr}");
+    println!("Successfully downloaded file from datamap at: {addr}");
+    Ok(())
 }

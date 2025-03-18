maidsafe/safe_network

feat(launchpad): add node and locking mechanism

node-launchpad/src/node_mgmt.rs
@@ -22,6 +22,8 @@ const NODE_ADD_MAX_RETRIES: u32 = 5;
 pub const FIXED_INTERVAL: u64 = 60_000;
 pub const CONNECTION_TIMEOUT_START: u64 = 120;
 
+pub const NODES_ALL: &str = "NODES_ALL";
+
 #[derive(Debug)]
 pub enum NodeManagementTask {
     MaintainNodes {
@@ -39,7 +41,7 @@ pub enum NodeManagementTask {
         args: UpgradeNodesArgs,
     },
     AddNode {
-        action_sender: UnboundedSender<Action>,
+        args: MaintainNodesArgs,
     },
     RemoveNodes {
         services: Vec<String>,
@@ -92,9 +94,7 @@ impl NodeManagement {
                             services,
                             action_sender,
                         } => start_nodes(services, action_sender).await,
-                        NodeManagementTask::AddNode { action_sender } => {
-                            // add_node(action_sender).await
-                        }
+                        NodeManagementTask::AddNode { args } => add_node(args).await,
                     }
                 }
                 // If the while loop returns, then all the LocalSpawner
@@ -127,21 +127,27 @@ impl NodeManagement {
 /// Stop the specified services
 async fn stop_nodes(services: Vec<String>, action_sender: UnboundedSender<Action>) {
     if let Err(err) =
-        sn_node_manager::cmd::node::stop(None, vec![], services, VerbosityLevel::Minimal).await
+        sn_node_manager::cmd::node::stop(None, vec![], services.clone(), VerbosityLevel::Minimal)
+            .await
     {
         error!("Error while stopping services {err:?}");
         send_action(
             action_sender,
             Action::StatusActions(StatusActions::ErrorStoppingNodes {
+                services,
                 raw_error: err.to_string(),
             }),
         );
     } else {
         info!("Successfully stopped services");
-        send_action(
-            action_sender,
-            Action::StatusActions(StatusActions::StopNodesCompleted),
-        );
+        for service in services {
+            send_action(
+                action_sender.clone(),
+                Action::StatusActions(StatusActions::StopNodesCompleted {
+                    service_name: service,
+                }),
+            );
+        }
     }
 }
 
@@ -200,7 +206,9 @@ async fn maintain_n_running_nodes(args: MaintainNodesArgs) {
     debug!("Finished maintaining {} nodes", args.count);
     send_action(
         args.action_sender,
-        Action::StatusActions(StatusActions::StartNodesCompleted),
+        Action::StatusActions(StatusActions::StartNodesCompleted {
+            service_name: NODES_ALL.to_string(),
+        }),
     );
 }
 
@@ -279,38 +287,113 @@ async fn remove_nodes(services: Vec<String>, action_sender: UnboundedSender<Acti
         send_action(
             action_sender,
             Action::StatusActions(StatusActions::ErrorRemovingNodes {
+                services,
                 raw_error: err.to_string(),
             }),
         );
     } else {
         info!("Successfully removed services {:?}", services);
-        send_action(
-            action_sender,
-            Action::StatusActions(StatusActions::RemovingNodesCompleted),
-        );
+        for service in services {
+            send_action(
+                action_sender.clone(),
+                Action::StatusActions(StatusActions::RemoveNodesCompleted {
+                    service_name: service,
+                }),
+            );
+        }
     }
 }
 
-// async fn add_node(action_sender: UnboundedSender<Action>) {
-//     if let Err(err) =
-//         sn_node_manager::cmd::node::add(false, vec![], services.clone(), VerbosityLevel::Minimal)
-//             .await
-//     {
-//         error!("Error while removing services {err:?}");
-//         send_action(
-//             action_sender,
-//             Action::StatusActions(StatusActions::ErrorRemovingNodes {
-//                 raw_error: err.to_string(),
-//             }),
-//         );
-//     } else {
-//         info!("Successfully removed services {:?}", services);
-//         send_action(
-//             action_sender,
-//             Action::StatusActions(StatusActions::RemovingNodesCompleted),
-//         );
-//     }
-// }
+async fn add_node(args: MaintainNodesArgs) {
+    debug!("Adding node");
+
+    if args.run_nat_detection {
+        run_nat_detection(&args.action_sender).await;
+    }
+
+    let config = prepare_node_config(&args);
+
+    let node_registry = match load_node_registry(&args.action_sender).await {
+        Ok(registry) => registry,
+        Err(err) => {
+            error!("Failed to load node registry: {:?}", err);
+            return;
+        }
+    };
+    let used_ports = get_used_ports(&node_registry);
+    let (mut current_port, max_port) = get_port_range(&config.custom_ports);
+
+    while used_ports.contains(&current_port) && current_port <= max_port {
+        current_port += 1;
+    }
+
+    if current_port > max_port {
+        error!("Reached maximum port number. Unable to find an available port.");
+        send_action(
+            args.action_sender.clone(),
+            Action::StatusActions(StatusActions::ErrorAddingNodes {
+                raw_error: format!(
+                    "When adding a new node we reached maximum port number ({}).\nUnable to find an available port.",
+                    max_port
+                ),
+            }),
+        );
+    }
+
+    let port_range = Some(PortRange::Single(current_port));
+    match sn_node_manager::cmd::node::add(
+        false, // auto_restart,
+        config.auto_set_nat_flags,
+        Some(config.count),
+        config.data_dir_path,
+        true, // enable_metrics_server,
+        None, // env_variables,
+        None, // evm_network
+        config.home_network,
+        false,      // local,
+        None,       // log_dir_path,
+        None,       // log_format,
+        None,       // max_archived_log_files,
+        None,       // max_log_files,
+        None,       // metrics_port,
+        None,       // node_ip,
+        port_range, // node_port
+        config.owner.clone(),
+        config.peers_args.clone(),
+        RewardsAddress::from_str(config.rewards_address.as_str()).unwrap(),
+        None,                         // rpc_address,
+        None,                         // rpc_port,
+        config.safenode_path.clone(), // src_path,
+        config.upnp,
+        None, // url,
+        None, // user,
+        None, // version,
+        VerbosityLevel::Minimal,
+    )
+    .await
+    {
+        Err(err) => {
+            error!("Error while adding services {err:?}");
+            send_action(
+                args.action_sender,
+                Action::StatusActions(StatusActions::ErrorAddingNodes {
+                    raw_error: err.to_string(),
+                }),
+            );
+        }
+        Ok(services) => {
+            info!("Successfully added services: {:?}", services);
+            for service in services {
+                send_action(
+                    args.action_sender.clone(),
+                    Action::StatusActions(StatusActions::AddNodesCompleted {
+                        service_name: service,
+                    }),
+                );
+            }
+        }
+    }
+}
 
 async fn start_nodes(services: Vec<String>, action_sender: UnboundedSender<Action>) {
     debug!("Starting node {:?}", services);
@@ -327,15 +410,20 @@ async fn start_nodes(services: Vec<String>, action_sender: UnboundedSender<Actio
         send_action(
             action_sender,
             Action::StatusActions(StatusActions::ErrorStartingNodes {
+                services,
                 raw_error: err.to_string(),
             }),
         );
     } else {
         info!("Successfully started services {:?}", services);
-        send_action(
-            action_sender,
-            Action::StatusActions(StatusActions::StartNodesCompleted),
-        );
+        for service in services {
+            send_action(
+                action_sender.clone(),
+                Action::StatusActions(StatusActions::StartNodesCompleted {
+                    service_name: service,
+                }),
+            );
+        }
     }
 }
 

vphongph/maidsafe_autonomi

node-launchpad/src/node_mgmt.rs
@@ -22,6 +22,8 @@ const NODE_ADD_MAX_RETRIES: u32 = 5;
 pub const FIXED_INTERVAL: u64 = 60_000;
 pub const CONNECTION_TIMEOUT_START: u64 = 120;
 
+pub const NODES_ALL: &str = "NODES_ALL";
+
 #[derive(Debug)]
 pub enum NodeManagementTask {
     MaintainNodes {
@@ -39,7 +41,7 @@ pub enum NodeManagementTask {
         args: UpgradeNodesArgs,
     },
     AddNode {
-        action_sender: UnboundedSender<Action>,
+        args: MaintainNodesArgs,
     },
     RemoveNodes {
         services: Vec<String>,
@@ -92,9 +94,7 @@ impl NodeManagement {
                             services,
                             action_sender,
                         } => start_nodes(services, action_sender).await,
-                        NodeManagementTask::AddNode { .. } => {
-                            // add_node(action_sender).await
-                        }
+                        NodeManagementTask::AddNode { args } => add_node(args).await,
                     }
                 }
                 // If the while loop returns, then all the LocalSpawner
@@ -127,21 +127,27 @@ impl NodeManagement {
 /// Stop the specified services
 async fn stop_nodes(services: Vec<String>, action_sender: UnboundedSender<Action>) {
     if let Err(err) =
-        ant_node_manager::cmd::node::stop(None, vec![], services, VerbosityLevel::Minimal).await
+        ant_node_manager::cmd::node::stop(None, vec![], services.clone(), VerbosityLevel::Minimal)
+            .await
     {
         error!("Error while stopping services {err:?}");
         send_action(
             action_sender,
             Action::StatusActions(StatusActions::ErrorStoppingNodes {
+                services,
                 raw_error: err.to_string(),
             }),
         );
     } else {
         info!("Successfully stopped services");
-        send_action(
-            action_sender,
-            Action::StatusActions(StatusActions::StopNodesCompleted),
-        );
+        for service in services {
+            send_action(
+                action_sender.clone(),
+                Action::StatusActions(StatusActions::StopNodesCompleted {
+                    service_name: service,
+                }),
+            );
+        }
     }
 }
 
@@ -201,7 +207,9 @@ async fn maintain_n_running_nodes(args: MaintainNodesArgs) {
     debug!("Finished maintaining {} nodes", args.count);
     send_action(
         args.action_sender,
-        Action::StatusActions(StatusActions::StartNodesCompleted),
+        Action::StatusActions(StatusActions::StartNodesCompleted {
+            service_name: NODES_ALL.to_string(),
+        }),
     );
 }
 
@@ -286,38 +294,112 @@ async fn remove_nodes(services: Vec<String>, action_sender: UnboundedSender<Acti
         send_action(
             action_sender,
             Action::StatusActions(StatusActions::ErrorRemovingNodes {
+                services,
                 raw_error: err.to_string(),
             }),
         );
     } else {
         info!("Successfully removed services {:?}", services);
-        send_action(
-            action_sender,
-            Action::StatusActions(StatusActions::RemovingNodesCompleted),
-        );
+        for service in services {
+            send_action(
+                action_sender.clone(),
+                Action::StatusActions(StatusActions::RemoveNodesCompleted {
+                    service_name: service,
+                }),
+            );
+        }
     }
 }
 
-// async fn add_node(action_sender: UnboundedSender<Action>) {
-//     if let Err(err) =
-//         sn_node_manager::cmd::node::add(false, vec![], services.clone(), VerbosityLevel::Minimal)
-//             .await
-//     {
-//         error!("Error while removing services {err:?}");
-//         send_action(
-//             action_sender,
-//             Action::StatusActions(StatusActions::ErrorRemovingNodes {
-//                 raw_error: err.to_string(),
-//             }),
-//         );
-//     } else {
-//         info!("Successfully removed services {:?}", services);
-//         send_action(
-//             action_sender,
-//             Action::StatusActions(StatusActions::RemovingNodesCompleted),
-//         );
-//     }
-// }
+async fn add_node(args: MaintainNodesArgs) {
+    debug!("Adding node");
+
+    if args.run_nat_detection {
+        run_nat_detection(&args.action_sender).await;
+    }
+
+    let config = prepare_node_config(&args);
+
+    let node_registry = match load_node_registry(&args.action_sender).await {
+        Ok(registry) => registry,
+        Err(err) => {
+            error!("Failed to load node registry: {:?}", err);
+            return;
+        }
+    };
+    let used_ports = get_used_ports(&node_registry);
+    let (mut current_port, max_port) = get_port_range(&config.custom_ports);
+
+    while used_ports.contains(&current_port) && current_port <= max_port {
+        current_port += 1;
+    }
+
+    if current_port > max_port {
+        error!("Reached maximum port number. Unable to find an available port.");
+        send_action(
+            args.action_sender.clone(),
+            Action::StatusActions(StatusActions::ErrorAddingNodes {
+                raw_error: format!(
+                    "When adding a new node we reached maximum port number ({}).\nUnable to find an available port.",
+                    max_port
+                ),
+            }),
+        );
+    }
+
+    let port_range = Some(PortRange::Single(current_port));
+    match ant_node_manager::cmd::node::add(
+        false, // auto_restart,
+        config.auto_set_nat_flags,
+        Some(config.count),
+        config.data_dir_path,
+        true, // enable_metrics_server,
+        None, // env_variables,
+        None, // evm_network
+        config.home_network,
+        None,       // log_dir_path,
+        None,       // log_format,
+        None,       // max_archived_log_files,
+        None,       // max_log_files,
+        None,       // metrics_port,
+        None,       // network_id
+        None,       // node_ip,
+        port_range, // node_port
+        config.peers_args.clone(),
+        RewardsAddress::from_str(config.rewards_address.as_str()).unwrap(),
+        None,                        // rpc_address,
+        None,                        // rpc_port,
+        config.antnode_path.clone(), // src_path,
+        config.upnp,
+        None, // url,
+        None, // user,
+        None, // version,
+        VerbosityLevel::Minimal,
+    )
+    .await
+    {
+        Err(err) => {
+            error!("Error while adding services {err:?}");
+            send_action(
+                args.action_sender,
+                Action::StatusActions(StatusActions::ErrorAddingNodes {
+                    raw_error: err.to_string(),
+                }),
+            );
+        }
+        Ok(services) => {
+            info!("Successfully added services: {:?}", services);
+            for service in services {
+                send_action(
+                    args.action_sender.clone(),
+                    Action::StatusActions(StatusActions::AddNodesCompleted {
+                        service_name: service,
+                    }),
+                );
+            }
+        }
+    }
+}
 
 async fn start_nodes(services: Vec<String>, action_sender: UnboundedSender<Action>) {
     debug!("Starting node {:?}", services);
@@ -334,15 +416,20 @@ async fn start_nodes(services: Vec<String>, action_sender: UnboundedSender<Actio
         send_action(
             action_sender,
             Action::StatusActions(StatusActions::ErrorStartingNodes {
+                services,
                 raw_error: err.to_string(),
             }),
         );
     } else {
         info!("Successfully started services {:?}", services);
-        send_action(
-            action_sender,
-            Action::StatusActions(StatusActions::StartNodesCompleted),
-        );
+        for service in services {
+            send_action(
+                action_sender.clone(),
+                Action::StatusActions(StatusActions::StartNodesCompleted {
+                    service_name: service,
+                }),
+            );
+        }
     }
 }
 
8812713f4a322594509858870ca22f8ac87477f0
Warm Beer <mickvd99@gmail.com>
2025-03-20 15:08:36 +0100

    chore: add copyright

--- autonomi/tests/network.rs
+++ autonomi/tests/network.rs
@@ -1,3 +1,11 @@
+// Copyright 2025 MaidSafe.net limited.
+//
+// This SAFE Network Software is licensed to you under The General Public License (GPL), version 3.
+// Unless required by applicable law or agreed to in writing, the SAFE Network Software distributed
+// under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied. Please review the Licences for the specific language governing
+// permissions and limitations relating to use of the SAFE Network Software.
+
 use ant_bootstrap::InitialPeersConfig;
 use ant_node::spawn::network_spawner::NetworkSpawner;
 use autonomi::{Client, ClientConfig, ClientOperatingStrategy};

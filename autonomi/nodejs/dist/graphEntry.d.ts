b24fb850494eb9a12541cec1841a9212cc67ad4d
Benno Zeeman <bzeeman@live.nl>
2025-06-17 09:50:39 +0200

    chore: remove stub Typescript definitions

--- autonomi/nodejs/dist/graphEntry.d.ts
+++
@@ -1,9 +0,0 @@
-import { GraphEntryOptions, PaymentOption } from './types';
-export declare class GraphEntry {
-    private nativeList;
-    private constructor();
-    static create(address: string): Promise<GraphEntry>;
-    get(): Promise<any[]>;
-    put(options: GraphEntryOptions, payment: PaymentOption): Promise<void>;
-    getCost(key: string): Promise<string>;
-}

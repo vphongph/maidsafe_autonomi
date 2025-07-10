b6bfc3a822dd90a7521d8ebfdb9f4c1c2de6d022
qima <qi.ma@maidsafe.net>
2025-07-10 20:17:01 +0800

    feat(cli): print download progress to stdout

--- autonomi/src/client/data_types/chunk.rs
+++ autonomi/src/client/data_types/chunk.rs
@@ -395,45 +395,55 @@ impl Client {
 
     /// Fetch and decrypt all chunks in the data map.
     pub(crate) async fn fetch_from_data_map(&self, data_map: &DataMap) -> Result<Bytes, GetError> {
-        debug!("Fetching encrypted data chunks from data map {data_map:?}");
+        let total_chunks = data_map.infos().len();
+        #[cfg(feature = "loud")]
+        println!("Fetching {total_chunks} encrypted data chunks from network.");
+        debug!("Fetching {total_chunks} encrypted data chunks from data map {data_map:?}");
+
         let mut download_tasks = vec![];
-        for info in data_map.infos() {
+        for (i, info) in data_map.infos().into_iter().enumerate() {
             download_tasks.push(async move {
-                match self
-                    .chunk_get(&ChunkAddress::new(info.dst_hash))
-                    .await
-                    .inspect_err(|err| {
-                        error!(
-                            "Error fetching chunk {:?}: {err:?}",
-                            ChunkAddress::new(info.dst_hash)
-                        )
-                    }) {
-                    Ok(chunk) => Ok(EncryptedChunk {
-                        index: info.index,
-                        content: chunk.value,
-                    }),
+                let idx = i + 1;
+                let chunk_addr = ChunkAddress::new(info.dst_hash);
+                match self.chunk_get(&chunk_addr).await {
+                    Ok(chunk) => {
+                        #[cfg(feature = "loud")]
+                        println!("Successfully fetched chunk {idx}/{total_chunks}({chunk_addr:?})");
+                        info!("Successfully fetched chunk {idx}/{total_chunks}({chunk_addr:?})");
+                        Ok(EncryptedChunk {
+                            index: info.index,
+                            content: chunk.value,
+                        })
+                    }
                     Err(err) => {
+                        #[cfg(feature = "loud")]
+                        println!(
+                            "Error fetching chunk {idx}/{total_chunks}({chunk_addr:?}): {err:?}"
+                        );
                         error!(
-                            "Error fetching chunk {:?}: {err:?}",
-                            ChunkAddress::new(info.dst_hash)
+                            "Error fetching chunk {idx}/{total_chunks}({chunk_addr:?}): {err:?}"
                         );
                         Err(err)
                     }
                 }
             });
         }
-        debug!("Successfully fetched all the encrypted chunks");
         let encrypted_chunks =
             process_tasks_with_max_concurrency(download_tasks, *CHUNK_DOWNLOAD_BATCH_SIZE)
                 .await
                 .into_iter()
                 .collect::<Result<Vec<EncryptedChunk>, GetError>>()?;
+        #[cfg(feature = "loud")]
+        println!("Successfully fetched all {total_chunks} encrypted chunks");
+        debug!("Successfully fetched all {total_chunks} encrypted chunks");
 
         let data = decrypt_full_set(data_map, &encrypted_chunks).map_err(|e| {
             error!("Error decrypting encrypted_chunks: {e:?}");
             GetError::Decryption(crate::self_encryption::Error::SelfEncryption(e))
         })?;
-        debug!("Successfully decrypted all the chunks");
+        #[cfg(feature = "loud")]
+        println!("Successfully decrypted all {total_chunks} chunks");
+        debug!("Successfully decrypted all {total_chunks} chunks");
         Ok(data)
     }
 }

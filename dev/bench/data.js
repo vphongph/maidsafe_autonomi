4060e62b19fa982fe1b94cf28c6f75220debd3d7
github-action-benchmark <github@users.noreply.github.com>
2025-08-12 01:50:16 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for b32ac621dd170ef681d4c68d7bf521931613ee67

---
+++ dev/bench/data.js
@@ -0,0 +1,47 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1754963416302,
+  "repoUrl": "https://github.com/vphongph/maidsafe_autonomi",
+  "entries": {
+    "`safe files` benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "b32ac621dd170ef681d4c68d7bf521931613ee67",
+          "message": "Merge pull request #3154 from grumbach/uploads_using_streaming\n\nfeat(client): implement streaming downloads for file_download and fil…",
+          "timestamp": "2025-08-08T13:26:23Z",
+          "tree_id": "21fb6225af9aad10bff49c54883f3317b5379f84",
+          "url": "https://github.com/vphongph/maidsafe_autonomi/commit/b32ac621dd170ef681d4c68d7bf521931613ee67"
+        },
+        "date": 1754963409507,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ant files upload 1mb",
+            "value": 2.52165837405126,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files upload 10mb",
+            "value": 9.55853465243175,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files download",
+            "value": 26.68199711474792,
+            "unit": "MiB/s"
+          }
+        ]
+      }
+    ]
+  }
+}

3461d8de75962987aca74c24ac069f529a86a8ed
github-action-benchmark <github@users.noreply.github.com>
2025-08-12 01:51:32 +0000

    add Node memory (customSmallerIsBetter) benchmark result for b32ac621dd170ef681d4c68d7bf521931613ee67

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754963416302,
+  "lastUpdate": 1754963492879,
   "repoUrl": "https://github.com/vphongph/maidsafe_autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -39,6 +39,37 @@ window.BENCHMARK_DATA = {
             "name": "ant files download",
             "value": 26.68199711474792,
             "unit": "MiB/s"
+          }
+        ]
+      }
+    ],
+    "Node memory": [
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "b32ac621dd170ef681d4c68d7bf521931613ee67",
+          "message": "Merge pull request #3154 from grumbach/uploads_using_streaming\n\nfeat(client): implement streaming downloads for file_download and fil…",
+          "timestamp": "2025-08-08T13:26:23Z",
+          "tree_id": "21fb6225af9aad10bff49c54883f3317b5379f84",
+          "url": "https://github.com/vphongph/maidsafe_autonomi/commit/b32ac621dd170ef681d4c68d7bf521931613ee67"
+        },
+        "date": 1754963492586,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 87,
+            "unit": "MB"
           }
         ]
       }

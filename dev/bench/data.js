632038025ace0a5556e043ae580cb7dc24f162f4
github-action-benchmark <github@users.noreply.github.com>
2025-09-02 04:15:08 +0000

    add Node memory (customSmallerIsBetter) benchmark result for ff0556631d6e76e062e9b540bcbf8e394f55dbec

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1756786432069,
+  "lastUpdate": 1756786508494,
   "repoUrl": "https://github.com/vphongph/maidsafe_autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -103,6 +103,35 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/vphongph/maidsafe_autonomi/commit/b32ac621dd170ef681d4c68d7bf521931613ee67"
         },
         "date": 1754963492586,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 87,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ff0556631d6e76e062e9b540bcbf8e394f55dbec",
+          "message": "Merge pull request #3169 from maidsafe/dependabot/github_actions/actions/checkout-5\n\nchore(deps): bump actions/checkout from 4 to 5",
+          "timestamp": "2025-08-28T10:55:32Z",
+          "tree_id": "38885f3c04b41310d2d127cd133eff0ab183669b",
+          "url": "https://github.com/vphongph/maidsafe_autonomi/commit/ff0556631d6e76e062e9b540bcbf8e394f55dbec"
+        },
+        "date": 1756786508126,
         "tool": "customSmallerIsBetter",
         "benches": [
           {

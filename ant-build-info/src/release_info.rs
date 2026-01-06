4fbd220d07453b39aa0f8ce8c35084c295234012
Chris O'Neil <chriso83@protonmail.com>
2026-01-06 18:09:22 +0000

    chore(release): release candidate 2025.13.3.2
    This is a fake RC for testing automatic upgrades.

--- ant-build-info/src/release_info.rs
+++ ant-build-info/src/release_info.rs
@@ -1,4 +1,4 @@
 pub const RELEASE_YEAR: &str = "2025";
 pub const RELEASE_MONTH: &str = "12";
 pub const RELEASE_CYCLE: &str = "3";
-pub const RELEASE_CYCLE_COUNTER: &str = "1";
+pub const RELEASE_CYCLE_COUNTER: &str = "2";
